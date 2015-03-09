9abd68767aaed84c746e61e6d9b9da88e2d26f2b
Alex Williams <awilliams@alexwilliams.ca>
2015-03-09 06:11:00 +0000

    First commit - HTTPS ffi-bindings for PicoLisp

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Copyright (c) 2015 Alexander Williams, Unscramble <license@unscramble.jp>
+# MIT License
+
+set -u
+set -e
+
+git submodule init
+git submodule update
+
+cd vendor/neon
+  ./autogen.sh
+  ./configure --enable-shared
+  make
+cd -
+
+cd lib
+  rm -f libneon.so
+  ln -s ../vendor/neon/src/.libs/libneon.so libneon.so
+cd -
